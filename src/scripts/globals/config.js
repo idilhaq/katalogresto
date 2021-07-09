76c9ab5e3608d4b7b297df7b0f619c2d11b82d50
Muhammad Idil Haq Amir <6358621+idilhaq@users.noreply.github.com>
2021-07-09 08:58:07 +0700

    Finished tutorial

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: '191d1515548f792f345c91c196b39b28',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,25 +1,25 @@
-import TheMovieDbSource from '../../data/themoviedb-source';
+import FavoriteMovieIdb from '../../data/favoritemovie-idb';
 import { createMovieItemTemplate } from '../templates/template-creator';
-
-const Upcoming = {
+ 
+const Like = {
   async render() {
     return `
       <div class="content">
-        <h2 class="content__heading">Upcoming in Cinema</h2>
+        <h2 class="content__heading">Your Liked Movie</h2>
         <div id="movies" class="movies">
-
+ 
         </div>
       </div>
     `;
   },
-
+ 
   async afterRender() {
-    const movies = await TheMovieDbSource.upcomingMovies();
+    const movies = await FavoriteMovieIdb.getAllMovies();
     const moviesContainer = document.querySelector('#movies');
     movies.forEach((movie) => {
       moviesContainer.innerHTML += createMovieItemTemplate(movie);
     });
   },
 };
-
-export default Upcoming;
+ 
+export default Like;
